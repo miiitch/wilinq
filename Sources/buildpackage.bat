4797375ecbdd446b57225c38a5facf01427195b6
Michel Perfetti <miiitch@gmail.com>
2013-07-18 17:13:48 +0200

    build update to 0.1.0.2

--- Sources/buildpackage.bat
+++ Sources/buildpackage.bat
@@ -2,4 +2,4 @@ msbuild wilinq.sln /p:Configuration="Release" /p:Platform=AnyCPU /Rebuild
 
 mkdir ..\GeneratedPackages
 
-nuget pack WiLinq.nuspec -version 0.1.0.1 -symbols -Verbosity detailed -OutputDirectory ..\GeneratedPackages
+nuget pack WiLinq.nuspec -version 0.1.0.2 -symbols -Verbosity detailed -OutputDirectory ..\GeneratedPackages
