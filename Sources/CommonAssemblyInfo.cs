2dac7b5dee6c2f13eafbe78c499cddb9b88c8cce
Michel Perfetti <miiitch@gmail.com>
2015-09-04 13:27:42 +0200

    Add 'New<>' method to create typed workitems

--- Sources/CommonAssemblyInfo.cs
+++ Sources/CommonAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("WiLinq")]
-[assembly: AssemblyCopyright("Copyright © Michel Perfetti 2014")]
+[assembly: AssemblyCopyright("Copyright © Michel Perfetti 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.2.1.*")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.2.2.*")]
+[assembly: AssemblyFileVersion("0.2.2.0")]
 [assembly: AssemblyCompany("Michel Perfetti")]
